6a648dc9b61b7afcfc2781728821ed9322d5586e
tonny <prasantha.raju@ais.com>
2023-05-11 02:40:25 +0530

    first fork edit

--- Azure_Example_Full/variables.tf
+++ Azure_Example_Full/variables.tf
@@ -21,5 +21,5 @@ variable "vnet_cidr_prefix" {
 
 variable "subnet1_cidr_prefix" {
   type = string
-  description = "This variable defines address space for subnetnet"
+  description = "This variable defines address space for subnetnets"
 }
